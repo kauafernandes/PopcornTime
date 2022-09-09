673e87a34e8854562830934df7df364d5475f3b7
marcosviniciusrz <marcostcc2004@gmail.com>
2022-09-09 13:44:19 -0300

    postos feito
    criei listar_posts no insomnia

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -6,7 +6,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarPosts(request, response){
         try{
-            return response.status(200).json({confirma: "Posts"}); 
+            const sql='SELECT id_post, id_usuario, data_post, texto_post, imagem_post FROM posts;';
+            const posts = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: posts[0].lenght, message: posts[0]}); 
         } catch (error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
