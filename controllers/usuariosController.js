4a1052d8d4972a4070529174b0e5245ef1041b03
ThamiresV <110108642+ThamiresV@users.noreply.github.com>
2022-09-09 13:44:23 -0300

    usuários feito

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarUsuarios(request, response){
         try{
-            return response.status(200).json({confirma: "Usuarios"}); 
+            const sql='SELECT id_usuario, email_usuario, nome_usuario, senha_usuario, apelido_usuario, genero_usuario, data_nasc_usuario, id_genero, foto_usuario, ponto_usuario FROM usuarios;';
+            const usuarios = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: usuarios[0].lenght, message: usuarios[0]}); 
         } catch (error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
