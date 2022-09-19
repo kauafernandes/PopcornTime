9e81f830602773f036a24ef327e9a891c74d1b72
kauafernandes <110110220+kauafernandes@users.noreply.github.com>
2022-09-19 10:24:00 -0300

    Select reviews
    select da reviews

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -4,7 +4,10 @@ const db = require("../database/connection");
 module.exports = {
     async listarReviews(request, response){
         try{
-            return response.status(200).json({confirma: "Reviews"}); 
+            const sql = 'SELECT id_usuario, id_titulo, review, data_review FROM reviews;';
+            const reviews = await db.query(sql);
+
+            return response.status(200).json({confirma: 'Sucesso', nResults: reviews[0].length, message: reviews[0]}); 
         } catch (error){
             return response.status(500).json({confirma: 'Erro', message: error});
         }
