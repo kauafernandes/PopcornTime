c63f9d290b545f52011b2ea575f717c30210917c
marielle-ss <110108955+marielle-ss@users.noreply.github.com>
2022-09-23 13:50:25 -0300

    ...

--- controllers/titulosController.js
+++ controllers/titulosController.js
@@ -5,7 +5,7 @@ const db = require("../database/connection");
 module.exports = {
     async listarTitulos(request, response){
         try{
-            const sql= 'SELECT id_titulo, id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo FROM titulos;';
+            const sql= 'SELECT id_titulo, id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo, id_api FROM titulos;';
             const titulos = await db.query(sql);
 
             return response.status(200).json({confirma: 'Sucesso',  nResults: titulos[0].length, message: titulos[0]}); 
@@ -16,9 +16,9 @@ module.exports = {
 
     async create(request, response) {
         try {
-            const {id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo} = request.body;
-            const sql = 'INSERT INTO titulos (id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-            const values = [id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo];
+            const {id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo, id_api} = request.body;
+            const sql = 'INSERT INTO titulos (id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo, id_api) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+            const values = [id_genero, nome_titulo, duracao_titulo, sinopse_titulo, cartaz_titulo, temporadas_titulo, trailer_titulo, data_lancamento_titulo, id_api];
             const confirmacao = await db.query(sql, values);
             const id_titulo = confirmacao[0].insertId;
             return response.status(200).json({confirma: 'Sucesso', message: id_titulo});
